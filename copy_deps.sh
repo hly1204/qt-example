e72259b208db72ed2cb11c9852e043cfbf5a7a0f
hly1204 <hly1204@qq.com>
2025-04-03 00:30:47 +0800

    Fix workflow

---
+++ copy_deps.sh
@@ -0,0 +1,4 @@
+release_dir=$1
+ldd $release_dir/bin/*.exe|grep -iv system32|grep -vi windows|grep -v :$  | cut -f2 -d\> | cut -f1 -d\( | tr '\\' '\/' |while read a; do ! [ -e "$release_dir/bin/`basename $a`" ] && cp -v "$a" $release_dir/bin/; done
+ldd $release_dir/bin/*.dll|grep -iv system32|grep -vi windows|grep -v :$  | cut -f2 -d\> | cut -f1 -d\( | tr '\\' '\/' |while read a; do ! [ -e "$release_dir/bin/`basename $a`" ] && cp -v "$a" $release_dir/bin/; done
+ldd $release_dir/plugins/*/*.dll|grep -iv system32|grep -vi windows|grep -v :$  | cut -f2 -d\> | cut -f1 -d\( | tr '\\' '\/' |while read a; do ! [ -e "$release_dir/bin/`basename $a`" ] && cp -v "$a" $release_dir/bin/; done
