249c8762671a79613811a3600513b3918e0223a8
hly1204 <hly1204@qq.com>
2025-07-05 15:50:03 +0800

    Minor

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QtLogging>
 #include <QString>
 #include <QLibraryInfo>
+#include <QDir>
 
 #include <cstdio>
 
@@ -11,34 +12,47 @@
 using namespace Qt::Literals::StringLiterals;
 
 namespace {
-
 struct LogToFile
 {
     LogToFile()
     {
         qSetMessagePattern("[%{time yyyyMMdd h:mm:ss.zzz ttt} "
-                           "%{if-debug}D%{endif}%{if-info}I%{endif}%{if-warning}W%{endif}"
-                           "%{if-critical}C%{endif}%{if-fatal}F%{endif}] "
+                           "%{if-debug}DEBUG%{endif}"
+                           "%{if-info} INFO%{endif}"
+                           "%{if-warning} WARN%{endif}"
+                           "%{if-critical} CRIT%{endif}"
+                           "%{if-fatal}FATAL%{endif}] "
 #if !defined(NDEBUG)
                            "%{file}:%{line} - "
 #endif
                            "%{message}"_L1);
-        // Note that it depends on the working directory
-        f_ = std::fopen("log.txt", "a");
-        o_ = qInstallMessageHandler(&LogToFile::logToFile);
+        if (QDir().mkpath("log"_L1)) {
+            const QString file_name =
+                    u"log/%1.txt"_s.arg(QDateTime::currentDateTime().toString("yyyy-MM-dd"_L1));
+            f_ = std::fopen(qPrintable(file_name), "a");
+            if (f_) {
+                o_ = qInstallMessageHandler(&LogToFile::logToFile);
+            }
+        }
     }
     ~LogToFile()
     {
-        qInstallMessageHandler(o_);
-        o_ = nullptr;
-        std::fclose(f_);
+        if (o_) {
+            qInstallMessageHandler(o_);
+            o_ = nullptr;
+        }
+        if (f_) {
+            std::fclose(f_);
+        }
     }
 
     static void logToFile(QtMsgType type, const QMessageLogContext &context, const QString &msg)
     {
-        const QString message = qFormatLogMessage(type, context, msg);
-        std::fprintf(f_, "%s\n", qPrintable(message));
-        std::fflush(f_);
+        if (f_) {
+            const QString message = qFormatLogMessage(type, context, msg);
+            std::fprintf(f_, "%s\n", qPrintable(message));
+            std::fflush(f_);
+        }
 
         if (o_) {
             (*o_)(type, context, msg);
@@ -48,18 +62,18 @@ struct LogToFile
     static inline QtMessageHandler o_;
     static inline std::FILE *f_;
 } _init [[maybe_unused]];
-
 } // namespace
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QTranslator t;
-    if (t.load(QLocale(), "qt"_L1, "_"_L1, QLibraryInfo::path(QLibraryInfo::TranslationsPath))) {
-        QCoreApplication::installTranslator(&t);
+    QTranslator translator;
+    if (translator.load(QLocale(), "qt"_L1, "_"_L1,
+                        QLibraryInfo::path(QLibraryInfo::TranslationsPath))) {
+        QCoreApplication::installTranslator(&translator);
     } else {
-        qWarning("Failed to load translator");
+        qWarning("Failed to load translations");
     }
 
     MainWindow win;
